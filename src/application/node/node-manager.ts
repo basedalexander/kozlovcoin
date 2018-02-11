aaa926cbe1fcdc22dcda2b6d95a2c60e066ae1ed
thohoh <itrefak@gmail.com>
2018-02-11 17:40:03 +0200

    #20 Moved business logic out of p2p message handler

--- src/application/node/node-manager.ts
+++ src/application/node/node-manager.ts
@@ -1,15 +1,22 @@
-import { Component } from '@nestjs/common';
+import { Component, Inject } from '@nestjs/common';
 import { IBlock } from '../block/block.interface';
 import { Node } from './node';
 import { UnspentTransactionOutput } from '../transaction/classes/unspent-transaction-output';
 import { Transaction } from '../transaction/classes/transaction';
 import { P2PNetwork } from '../p2p-network/p2p-network';
+import { ILogger, TLogger } from '../../system/logger/interfaces/logger.interface';
+import { BlockValidatorService } from '../block/block-validator.service';
+import { P2PMessageType } from '../p2p-network/messages/interfaces/p2p-message-type';
+import { P2PMessageFactory } from '../p2p-network/messages/p2p-message-factory';
 
 @Component()
 export class NodeManager {
     constructor(
         private node: Node,
-        private p2p: P2PNetwork
+        private p2p: P2PNetwork,
+        @Inject(TLogger) private logger: ILogger,
+        private blockValidator: BlockValidatorService,
+        private messageFactory: P2PMessageFactory
     ) { }
 
     async getBlocks(): Promise<IBlock[]> {
@@ -39,5 +46,33 @@ export class NodeManager {
 
     async mineNewBlock(): Promise<IBlock> {
         return await this.node.mineNewBlock();
+    }
+
+    async handleReceivedLastBlock(block: IBlock, sender: WebSocket): Promise<void> {
+        if (!this.blockValidator.validateStructure(block)) {
+            this.logger.error('Received last block has invalid structure, ignoring');
+            return;
+        }
+
+        const heldLastBlock: IBlock = await this.node.getLastBlock();
+
+        if (block.index > heldLastBlock.index) {
+            if (block.previousBlockHash === heldLastBlock.hash) {
+                await this.node.addNewBlock(block);
+
+                const msg = this.messageFactory.createMessage(
+                    P2PMessageType.RESPONSE_LAST_BLOCK,
+                    block
+                );
+                this.p2p.broadcast(msg);
+            } else {
+                this.logger.error(`Node: Adding new block, received block ${block.hash} either higher then our last block or not valid`);
+
+                const msg = this.messageFactory.createMessage(
+                    P2PMessageType.QUERY_ALL_BLOCKS
+                );
+                this.p2p.sendMessage(sender, msg);
+            }
+        }
     }
 }
--- src/application/p2p-network/messages/message-descriptors/response-latest-block/response-latest-block.p2p-message-handler.ts
+++ src/application/p2p-network/messages/message-descriptors/response-latest-block/response-latest-block.p2p-message-handler.ts
@@ -4,46 +4,21 @@ import { P2PNetwork } from '../../../p2p-network';
 import { ILogger, TLogger } from '../../../../../system/logger/interfaces/logger.interface';
 import { IP2PMessageHandler } from '../../interfaces/message-handler.interface';
 import { IP2PMessage } from '../../interfaces/p2p-message.interface';
-import { P2PMessageType } from '../../interfaces/p2p-message-type';
-import { P2PMessageFactory } from '../../p2p-message-factory';
-import { BlockValidatorService } from '../../../../block/block-validator.service';
+import { IBlock } from '../../../../block/block.interface';
+import { NodeManager } from '../../../../node/node-manager';
 
 @Component()
 export class ResponseLatestBlockP2PMessageHandler implements IP2PMessageHandler {
     constructor(private node: Node,
                 private p2p: P2PNetwork,
                 @Inject(TLogger) private logger: ILogger,
-                private messageFactory: P2PMessageFactory,
-                private blockValidator: BlockValidatorService) {
+                private nodeManager: NodeManager
+    ) {
     }
 
     async execute(ws, message: IP2PMessage): Promise<void> {
-        const receivedLatestBlock = message.data;
+        const block: IBlock = message.data;
 
-        if (!this.blockValidator.validateStructure(receivedLatestBlock)) {
-            this.logger.error('Received last block has invalid structure, ignoring');
-            return;
-        }
-
-        const heldLatestBlock = await this.node.getLastBlock();
-
-        if (receivedLatestBlock.index > heldLatestBlock.index) {
-            if (receivedLatestBlock.previousBlockHash === heldLatestBlock.hash) {
-                await this.node.addNewBlock(receivedLatestBlock);
-
-                const msg = this.messageFactory.createMessage(
-                    P2PMessageType.RESPONSE_LAST_BLOCK,
-                    receivedLatestBlock
-                );
-                this.p2p.broadcast(msg);
-            } else {
-                this.logger.error(`Node: Adding new block, received block ${receivedLatestBlock.hash} either higher then our last block or not valid`);
-
-                const msg = this.messageFactory.createMessage(
-                    P2PMessageType.QUERY_ALL_BLOCKS
-                );
-                this.p2p.sendMessage(ws, msg);
-            }
-        }
+        await this.nodeManager.handleReceivedLastBlock(block, ws);
     }
 }
